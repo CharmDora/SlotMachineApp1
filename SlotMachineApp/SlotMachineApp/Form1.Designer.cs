5d1398d4e417a5b63dd27d85e7d5556a13574197
CharmDora <152815035+CharmDora@users.noreply.github.com>
2023-12-04 00:34:13 +0300

    Add files via upload

---
+++ SlotMachineApp/SlotMachineApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace SlotMachineApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.lblSlot1 = new System.Windows.Forms.Label();
+            this.lblSlot2 = new System.Windows.Forms.Label();
+            this.lblSlot3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSpin
+            // 
+            this.btnSpin.BackColor = System.Drawing.Color.Yellow;
+            this.btnSpin.Font = new System.Drawing.Font("Stencil", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpin.Location = new System.Drawing.Point(360, 470);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(263, 92);
+            this.btnSpin.TabIndex = 0;
+            this.btnSpin.Text = "ÇEVİR";
+            this.btnSpin.UseVisualStyleBackColor = false;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click_1);
+            // 
+            // lblSlot1
+            // 
+            this.lblSlot1.AutoSize = true;
+            this.lblSlot1.BackColor = System.Drawing.Color.Lime;
+            this.lblSlot1.Font = new System.Drawing.Font("Stencil", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSlot1.Location = new System.Drawing.Point(246, 181);
+            this.lblSlot1.Name = "lblSlot1";
+            this.lblSlot1.Size = new System.Drawing.Size(108, 114);
+            this.lblSlot1.TabIndex = 1;
+            this.lblSlot1.Text = "A";
+            // 
+            // lblSlot2
+            // 
+            this.lblSlot2.AutoSize = true;
+            this.lblSlot2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblSlot2.Font = new System.Drawing.Font("Stencil", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSlot2.Location = new System.Drawing.Point(440, 181);
+            this.lblSlot2.Name = "lblSlot2";
+            this.lblSlot2.Size = new System.Drawing.Size(108, 114);
+            this.lblSlot2.TabIndex = 2;
+            this.lblSlot2.Text = "A";
+            // 
+            // lblSlot3
+            // 
+            this.lblSlot3.AutoSize = true;
+            this.lblSlot3.BackColor = System.Drawing.Color.Cyan;
+            this.lblSlot3.Font = new System.Drawing.Font("Stencil", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSlot3.Location = new System.Drawing.Point(629, 181);
+            this.lblSlot3.Name = "lblSlot3";
+            this.lblSlot3.Size = new System.Drawing.Size(108, 114);
+            this.lblSlot3.TabIndex = 3;
+            this.lblSlot3.Text = "A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(962, 634);
+            this.Controls.Add(this.lblSlot3);
+            this.Controls.Add(this.lblSlot2);
+            this.Controls.Add(this.lblSlot1);
+            this.Controls.Add(this.btnSpin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpin;
+        private System.Windows.Forms.Label lblSlot1;
+        private System.Windows.Forms.Label lblSlot2;
+        private System.Windows.Forms.Label lblSlot3;
+
+        public EventHandler Form1_Load { get; private set; }
+    }
+}
+
---
+++ SlotMachineApp/SlotMachineApp/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SlotMachineApp
+{
+    partial class Form1 : Form
+    {
+        // Slot makinesi sembolleri (örneğin, harfler)
+        private string[] symbols = { "A", "B", "C", "D", "E", "F" };
+
+        // Rastgele sayı üretici
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpin_Click(object sender, EventArgs e)
+        {
+            SpinSlotMachine();
+        }
+
+        private void SpinSlotMachine()
+        {
+            // Her Slotta rastgele bir sembol seç
+            string symbol1 = GetRandomSymbol();
+            string symbol2 = GetRandomSymbol();
+            string symbol3 = GetRandomSymbol();
+
+            // Slotlara sembolleri yerleştir
+            lblSlot1.Text = symbol1;
+            lblSlot2.Text = symbol2;
+            lblSlot3.Text = symbol3;
+
+            // Kazanıp kazanmadığını kontrol et
+            CheckForWin(symbol1, symbol2, symbol3);
+        }
+
+        private string GetRandomSymbol()
+        {
+            // Rastgele bir sembol seç
+            int index = random.Next(symbols.Length);
+            return symbols[index];
+        }
+
+        private void CheckForWin(string symbol1, string symbol2, string symbol3)
+        {
+            // Üç sembol de aynıysa kazanmıştır
+            if (symbol1 == symbol2 && symbol2 == symbol3)
+            {
+                MessageBox.Show("Tebrikler! Kazandınız!", "Slot Makinesi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Üzgünüz, bu sefer kazanamadınız. Tekrar deneyin!", "Slot Makinesi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSpin_Click_1(object sender, EventArgs e)
+        {
+            SpinSlotMachine();
+        }
+    }
+}
